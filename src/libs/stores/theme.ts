47465c4a59dbad7b968f9073d9eff39fba58c51d
aksworns22 <wkdeogks17@gmail.com>
2026-04-23 00:12:35 +0900

    chore: 항상 다크모드를 유지하도록 변경

--- src/libs/stores/theme.ts
+++ src/libs/stores/theme.ts
@@ -1,60 +1,60 @@
-import { persistentAtom } from '@nanostores/persistent';
-import { onMount } from 'nanostores';
+// import { persistentAtom } from '@nanostores/persistent';
+// import { onMount } from 'nanostores';
 
-import { changeGiscusTheme } from '~/components/giscus';
+// import { changeGiscusTheme } from '~/components/giscus';
 
-export const THEME_MAP = {
-  light: 'light',
-  dark: 'dark',
-  system: undefined,
-} as const;
+// export const THEME_MAP = {
+//   light: 'light',
+//   dark: 'dark',
+//   system: undefined,
+// } as const;
 
-export type ThemeKey = keyof typeof THEME_MAP;
-export type ThemeValue = (typeof THEME_MAP)[ThemeKey];
+// export type ThemeKey = keyof typeof THEME_MAP;
+// export type ThemeValue = (typeof THEME_MAP)[ThemeKey];
 
-export const STORAGE_THEME_KEY = 'theme' as const;
+// export const STORAGE_THEME_KEY = 'theme' as const;
 
-export const themeStore = persistentAtom<ThemeValue>(
-  STORAGE_THEME_KEY,
-  THEME_MAP.system,
-);
+// export const themeStore = persistentAtom<ThemeValue>(
+//   STORAGE_THEME_KEY,
+//   THEME_MAP.dark,
+// );
 
-const initThemeStoreSubscribe = () => {
-  const applyTheme = (theme: ThemeValue) => {
-    if (theme === THEME_MAP.dark) {
-      changeGiscusTheme('dark');
-      document.documentElement.classList.add('dark');
-    } else if (theme === THEME_MAP.light) {
-      changeGiscusTheme('light');
-      document.documentElement.classList.remove('dark');
-    }
-  };
+// const initThemeStoreSubscribe = () => {
+//   const applyTheme = (theme: ThemeValue) => {
+//     if (theme === THEME_MAP.dark) {
+//       changeGiscusTheme('dark');
+//       document.documentElement.classList.add('dark');
+//     } else if (theme === THEME_MAP.light) {
+//       changeGiscusTheme('light');
+//       document.documentElement.classList.remove('dark');
+//     }
+//   };
 
-  const handleMediaQuery = (query: { matches: boolean }) => {
-    applyTheme(query.matches ? THEME_MAP.dark : THEME_MAP.light);
-  };
+//   const handleMediaQuery = (query: { matches: boolean }) => {
+//     applyTheme(query.matches ? THEME_MAP.dark : THEME_MAP.light);
+//   };
 
-  themeStore.subscribe((theme) => {
-    if (theme !== THEME_MAP.system) {
-      applyTheme(theme);
-      return;
-    }
+//   themeStore.subscribe((theme) => {
+//     if (theme !== THEME_MAP.system) {
+//       applyTheme(theme);
+//       return;
+//     }
 
-    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+//     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
 
-    if (mediaQuery.addEventListener) {
-      mediaQuery.removeEventListener('change', handleMediaQuery);
-      mediaQuery.addEventListener('change', handleMediaQuery);
-    } else {
-      mediaQuery.removeListener(handleMediaQuery);
-      mediaQuery.addListener(handleMediaQuery);
-    }
-    handleMediaQuery(mediaQuery);
-  });
-};
+//     if (mediaQuery.addEventListener) {
+//       mediaQuery.removeEventListener('change', handleMediaQuery);
+//       mediaQuery.addEventListener('change', handleMediaQuery);
+//     } else {
+//       mediaQuery.removeListener(handleMediaQuery);
+//       mediaQuery.addListener(handleMediaQuery);
+//     }
+//     handleMediaQuery(mediaQuery);
+//   });
+// };
 
-if (typeof window !== 'undefined') {
-  onMount(themeStore, () => {
-    initThemeStoreSubscribe();
-  });
-}
+// if (typeof window !== 'undefined') {
+//   onMount(themeStore, () => {
+//     initThemeStoreSubscribe();
+//   });
+// }
